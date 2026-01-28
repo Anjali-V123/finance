91adef247967eeac34ffdd66fe478a4e6468aa04
Anjali V <ramaanjaliv5@gmail.com>
2026-01-28 19:54:58 +0530

    Update Signup to remove unwanted variables

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 function Signup() {
-  const [username, setUsername] = useState("");
+  //const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
